43b4f9fee4419ad610fa487880dcd2821cae0ef4
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2011-09-15 13:01:39 -0500

    Finish base implementation

--- demo/script.py
+++ demo/script.py
@@ -3,6 +3,8 @@ import os
 import readline
 import shlex
 import string
+import sys
+import traceback
 
 from demo import aliases
 
@@ -29,10 +31,10 @@ class ScriptLine(object):
         self.raw = line.strip()
 
         # Start from the raw input
-        line = self.raw()
+        line = self.raw
 
         # Inhibit output?
-        if line[0] == '!':
+        if line and line[0] == '!':
             self.output = False
             line = line[1:]
         else:
@@ -41,19 +43,23 @@ class ScriptLine(object):
         # Identify the line
         if lno == 1 and line[0:2] == '#!':
             self.type = 'comment'
-            self.subtype = 'script'
+            self.output = False
         elif line[0:2] == '##':
             self.type = 'comment'
-            self.subtype = 'invisible'
-        elif line[0] == '#':
+            self.output = False
+        elif line and line[0] == '#':
             self.type = 'comment'
-            self.subtype = 'comment'
+
+            # Take out leading '!'
+            if not self.output:
+                self.raw = line
+
+            self.output = True
         elif not line:
             self.type = 'pause'
-            self.subtype = None
+            self.output = False
         else:
             self.type = 'command'
-            self.subtype = None
 
         # OK, if it's not a command, let's stop here
         if self.type != 'command':
@@ -111,48 +117,25 @@ class ScriptLine(object):
         return alias.execute(ctx, self)
 
 
-def scriptfile(fname):
-    lno = 0
-    # Ignore leading blank lines that would be treated as pauses
-    inhibit_pause = True
-    with open(fname, 'r') as f:
-        for line in f:
-            # Track the line number
-            lno += 1
-
-            # Parse the line
-            sc_line = ScriptLine(fname, lno, line)
-
-            # Process pauses
-            if sc_line.type == 'pause':
-                if inhibit_pause:
-                    # Apply pause inhibition
-                    continue
-
-                # Inhibit future pausing
-                inhibit_pause = True
-            else:
-                # Not inhibiting any more pauses
-                inhibit_pause = False
-
-            # Add the line to the history
-            if sc_line.type in ('command', 'export'):
-                readline.add_history(sc_line.raw)
-
-            # Yield the line
-            yield sc_line
-
-
 class Script(object):
-    def __init__(self, fname, outfile=None, prompt='[%(nextcmd)s]> '):
+    def __init__(self, opts):
+        self.opts = opts
         self.exit_flag = False
-        self.filestack = [iter(scriptfile(fname))]
+
+        # Set up the filestack
+        self.filestack = []
+        self.empty = False
+        for fname in reversed(opts.files):
+            if fname == '-':
+                self.filestack.append((False, iter(self._read_input())))
+            else:
+                self.filestack.append((True, iter(self._read_file(fname))))
 
         # Was outfile specified?
-        self.outfile = None if outfile is None else open(outfile, 'w')
+        self.outfile = None if opts.output is None else open(opts.output, 'w')
 
         # Save the prompt template
-        self.prompt_tmpl = prompt
+        self.prompt_tmpl = opts.prompt
 
         # stdin line number
         self.in_lno = 1
@@ -163,24 +146,60 @@ class Script(object):
 
     def push_file(self, fname):
         # Push another file to process
-        self.filestack.append(iter(scriptfile(fname)))
+        self.filestack.append((True, iter(self._read_file(fname))))
 
-    def iter_lines(self):
+    def _prompt(self, input_prompt=False):
+        nextcmd = readline.get_current_history_length() + int(input_prompt)
+        currdir = os.getcwd()
+
+        return self.prompt_tmpl % locals()
+
+    def _iter_lines(self):
         while self.filestack:
             try:
                 # Yield the next line
-                yield next(self.filestack[-1])
+                yield (self.filestack[-1][0], next(self.filestack[-1][1]))
             except StopIteration:
                 self.filestack.pop(-1)
 
-    def read_input(self):
+    def _read_file(self, fname):
+        lno = 0
+        # Ignore leading blank lines that would be treated as pauses
+        inhibit_pause = True
+        with open(fname, 'r') as f:
+            for line in f:
+                # Track the line number
+                lno += 1
+
+                # Parse the line
+                sc_line = ScriptLine(fname, lno, line)
+
+                # Process pauses
+                if sc_line.type == 'pause':
+                    if inhibit_pause:
+                        # Apply pause inhibition
+                        continue
+
+                    # Inhibit future pausing
+                    inhibit_pause = True
+                else:
+                    # Not inhibiting any more pauses
+                    inhibit_pause = False
+
+                # Add the line to the history
+                if sc_line.type in ('command', 'export'):
+                    readline.add_history(sc_line.raw)
+
+                # Yield the line
+                yield sc_line
+
+    def _read_input(self):
         # Helper to generate the prompt
         def get_input():
-            lno = self.in_lno
-            nextcmd = readline.get_current_history_length() + 1
-            currdir = os.getcwd()
-
-            return raw_input(self.prompt_tmpl % locals()).strip()
+            try:
+                return raw_input(self._prompt(True)).strip()
+            except EOFError:
+                return ''
 
         line = get_input()
         while line:
@@ -188,8 +207,50 @@ class Script(object):
             tmp_lno = self.in_lno
             self.in_lno += 1
 
-            # Parse the line
-            yield ScriptLine('-', in_lno, line)
+            # Parse the line--should never yield a pause
+            yield ScriptLine('<stdin>', tmp_lno, line)
 
             # Get the next line
             line = get_input()
+
+    def execute(self):
+        recent_pause = False
+
+        for output, sc_line in self._iter_lines():
+            # Write the output script, but skip "source" instructions
+            if (self.outfile is not None and
+                (sc_line.type != 'command' or
+                 sc_line.args[0] != '.')):
+                print >>self.outfile, str(sc_line)
+                self.outfile.flush()
+
+            # Do we need to emit the line to stdout?
+            if output and sc_line.output:
+                print "%s%s" % (self._prompt(sc_line.type != 'command'),
+                                sc_line)
+
+            # Execute the line
+            try:
+                sc_line.execute(self)
+            except PauseCommand:
+                if not recent_pause:
+                    recent_pause = True
+                    self.filestack.append((False, iter(self._read_input())))
+                continue
+            except Exception, e:
+                print >>sys.stderr, ("Got exception %s at %s:%s" %
+                                     (e, sc_line.fname, sc_line.lno))
+                if self.opts.debug:
+                    traceback.print_exc()
+
+            recent_pause = False
+
+            # If we were instructed to exit, do so
+            if self.exit_flag:
+                return
+
+        # If we hit the end of input normally, throw on a final
+        if not recent_pause and not self.empty:
+            self.empty = True
+            self.filestack.append((False, iter(self._read_input())))
+            return self.execute()
